6060a3009dbe06d7e6b3c2ff7bf56f5a4e64095f
mrtnzlml <mrtnzlml@gmail.com>
2014-02-25 21:53:00 +0100

    improved router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
+use Nette;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
-use Nette;
 
 /**
  * Router factory.
@@ -35,6 +35,11 @@ class RouterFactory {
 		foreach ($this->broken_links as $key => $value) {
 			$router[] = new Route($key, $value, Route::ONE_WAY);
 		}
+		$router[] = new Route('last', array(
+			'presenter' => 'Homepage',
+			'action' => 'default',
+			'paginator-page' => ceil($pages / 8)
+		), Route::ONE_WAY);
 		$router[] = new Route('rss', 'Homepage:rss');
 		$router[] = new Route('sitemap.xml', 'Homepage:sitemap');
 		$router[] = new Route('admin', 'Admin:default');
