9b0a37d597359d8fa477d4be2f2daf1744eff550
Martin Zlámal <mrtnzlml@gmail.com>
2014-08-02 12:30:46 +0200

    improved PostMirror entity
    I have to wait for Doctrine 2.5...

--- app/model/Entity/PostMirror.php
+++ app/model/Entity/PostMirror.php
@@ -8,16 +8,19 @@ use Kdyby\Doctrine;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="mirror_posts", options={"engine"="MyISAM"})
+ * @ORM\Table(
+ *      name="mirror_posts",
+ *      options={"engine"="MyISAM"},
+ *      indexes={
+ *          @ORM\Index(columns={"title, body"}, flags={"fulltext"}),
+ *          @ORM\Index(columns={"title"}, flags={"fulltext"}),
+ *          @ORM\Index(columns={"body"}, flags={"fulltext"})
+ *      }
+ * )
+ * TODO: flags feature is available in Doctrine 2.5
+ * see: https://github.com/doctrine/doctrine2/commit/3a1e24e6801961128c27104919050d40d745030b
  */
 class PostMirror extends Doctrine\Entities\BaseEntity {
-
-	/* TODO: MyISAM
-		PRIMARY	id
-		FULLTEXT	title, body
-		FULLTEXT	title
-		FULLTEXT	body
-	 */
 
 	/**
 	 * @ORM\Id
