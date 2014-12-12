db1b2a5c46d930a8c484c9f3ef0cbe027948afe5
Martin Zlámal <mrtnzlml@gmail.com>
2014-12-12 11:59:40 +0100

    Travis-CI fix

--- sql/settings-pgsql.sql
+++ sql/settings-pgsql.sql
@@ -2,4 +2,5 @@ INSERT INTO settings (id, key, value) VALUES
 (1,	'show_content',	'1'),
 (2,	'random_search',	'1'),
 (3,	'show_comments',	'1'),
-(5,	'show_print',	'1');
+(4,	'show_print',	'1'),
+(5,	'disable_blog',	'0');
