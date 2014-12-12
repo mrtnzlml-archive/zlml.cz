00b20e94163cc125e31b6a34de0e918f26438d7e
Martin Zlámal <mrtnzlml@gmail.com>
2014-12-12 12:04:40 +0100

    added disqus_shortname

--- sql/settings-mysql.sql
+++ sql/settings-mysql.sql
@@ -3,4 +3,5 @@ REPLACE INTO `settings` (`id`, `key`, `value`) VALUES
 (2,	'random_search',	'1'),
 (3,	'show_comments',	'1'),
 (4,	'show_print',	'1'),
-(5,	'disable_blog',	'0');
+(5,	'disable_blog',	'0'),
+(6,	'disqus_shortname',	'');
