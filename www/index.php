1f2f6d9353040f7a5fca384121719eb7eb0ad6a7
Martin Zlámal <mrtnzlml@gmail.com>
2015-05-03 21:02:08 +0200

    better .maintenance page

--- www/index.php
+++ www/index.php
@@ -6,7 +6,9 @@
 // Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/../app/bootstrap.php';
 
-define('APP_DIR', __DIR__ . '/../app');
-
-// Run application.
-$container->getService('application')->run();
+try {
+	// Run application.
+	$container->getService('application')->run();
+} catch (\Doctrine\DBAL\Exception\ConnectionException $exc) {
+	require_once(__DIR__ . '/.maintenance.php');
+}
