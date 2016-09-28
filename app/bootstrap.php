662dcbfc0d7b63289417254a745837cbaf301ebc
Martin Zlámal <mrtnzlml@gmail.com>
2016-09-28 19:15:34 +0200

    Travis: run orm:validate-schema in debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,12 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new \Nette\Configurator;
+
+\Kdyby\Console\DI\BootstrapHelper::setupMode($configurator);
+if (getenv('NETTE_DEBUG') === '0' || getenv('NETTE_DEBUG') === '1') {
+	$configurator->setDebugMode((bool)getenv('NETTE_DEBUG'));
+}
+
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
