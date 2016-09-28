dbd966fc63e441f59860c77ad0d21d714f391744
Martin Zlámal <mrtnzlml@gmail.com>
2016-09-28 10:07:19 +0200

    Remove install scripts

--- sql/fulltext-workaround-mysql.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `mirror_posts`
-ADD FULLTEXT `title` (`title`),
-ADD FULLTEXT `body` (`body`),
-ADD FULLTEXT `title_body` (`title`, `body`);
--- sql/settings-pgsql.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO settings (id, key, value) VALUES
-(1,	'show_content',	'1'),
-(2,	'random_search',	'1'),
-(3,	'show_comments',	'1'),
-(4,	'show_print',	'1'),
-(5,	'disable_blog',	'0'),
-(6,	'disqus_shortname',	'');
