355d5368c335924610f24a6992dd3fda9059c468
Martin Zlámal <mrtnzlml@gmail.com>
2016-09-28 10:22:18 +0200

    Simplify bootstrap.php (remove install feature)

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -18,26 +18,4 @@ if (file_exists(__DIR__ . '/config/config.local.neon')) {
 	$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 }
 
-$help = <<<HELP
-
-Please configure connection to the database first! Use following options:
-
-	php index.php -u <db-user> -n <db-name> [-p <db-pass>] [-d <db-driver=mysql|pgsql>]
-
-HELP;
-
-if (file_exists(__DIR__ . '/config/config.local.neon')) {
-	$config = \Nette\Neon\Neon::decode(file_get_contents(__DIR__ . '/config/config.local.neon'));
-}
-if (isset($config) && is_array($config) && array_key_exists('doctrine', $config)) {
-	return $configurator->createContainer();
-} elseif (PHP_SAPI === 'cli') {
-	$options = getopt('u:n:p:d:');
-	if (!isset($options['u']) || !isset($options['n'])) {
-		die($help);
-	}
-	require_once(__DIR__ . '/model/.install-cli.php');
-	exit;
-} else {
-	require_once(__DIR__ . '/model/.install.php');
-}
+return $configurator->createContainer();
