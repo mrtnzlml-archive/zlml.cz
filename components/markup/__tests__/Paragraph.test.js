169f780176723fccdab240a5cbd9c4b40149b4c7
Martin Zlámal <mrtnzlml@gmail.com>
2017-10-14 20:56:32 -0500

    Add test for 'Paragraph' component

---
+++ components/markup/__tests__/Paragraph.test.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Paragraph from '../Paragraph';
+
+it('renders correctly', () => {
+  expect(
+    renderer.create(<Paragraph>TEST</Paragraph>).toJSON(),
+  ).toMatchSnapshot();
+});
