03d1b1f2ae62f42df1f9b4e30c14415098d27a66
Martin Zlámal <mrtnzlml@gmail.com>
2017-12-29 09:52:00 +0700

    Deploy application only on master branch

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# see: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -e # exit immediately if a pipeline returns a non-zero status
+set -x # print a trace of simple commands
+
+yarn install --production
+serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+serverless deploy --verbose
+curl -s --head  --request GET https://zlml.cz | grep "200 OK" > /dev/null || exit 1
